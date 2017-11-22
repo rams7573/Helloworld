806fbaea7d3e3d13c081ebb839dc88860f9db455
rams7573 <ramasubramanian.ka@inatech.com>
2017-11-22 12:51:37 +0530

    Integer Updated

--- src/main/java/Test1/Vehcile1.java
+++ src/main/java/Test1/Vehcile1.java
@@ -4,11 +4,12 @@ public class Vehcile1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a,b,c;
+		int a,b,c,d;
 a = 12;
 b =13;
-c = a + b;
-System.out.println("Sum of entered integers = "+c);
+c = 14;
+d = a + b +c;
+System.out.println("Sum of entered integers = "+d);
 	}
 
 }
